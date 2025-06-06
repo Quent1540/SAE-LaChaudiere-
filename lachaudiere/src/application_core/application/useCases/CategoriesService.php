170f0d9b0fe5f20432412221f18a92bc6989eea3
Quent1540 <dieudonnequent1@gmail.com>
2025-06-06 11:37:12 +0200

    liaison des services et interfaces dans bootstrap et dependencies.php pour corriger une erreur

--- lachaudiere/src/application_core/application/useCases/CategoriesService.php
+++ lachaudiere/src/application_core/application/useCases/CategoriesService.php
@@ -3,7 +3,7 @@ namespace lachaudiere\application_core\application\useCases;
 
 use lachaudiere\application_core\domain\entities\Categorie;
 
-class CategoriesService
+class CategoriesService implements CategoriesServiceInterface
 {
     public function getCategories(): \Illuminate\Database\Eloquent\Collection
     {
