8e8e72e974d69133513778697a1ed990fc3011e7
Quent1540 <dieudonnequent1@gmail.com>
2025-06-06 08:25:52 +0200

    ?

--- lachaudiere/src/application_core/application/useCases/AuthnService.php
+++ lachaudiere/src/application_core/application/useCases/AuthnService.php
@@ -8,19 +8,19 @@ use Ramsey\Uuid\Uuid;
 class AuthnService implements AuthnServiceInterface {
     public function register(string $email, string $password): bool
     {
-        if (User::query()->where('user_id', $email)->exists()) {
+        if (User::query()->where('email', $email)->exists()) {
             return false;
         }
         $user = new User();
         $user->id = Uuid::uuid4()->toString(); //On genere un UUID pour l'id de l'utilisateur
-        $user->user_id = $email;
+        $user->email = $email;
         $user->password = password_hash($password, PASSWORD_DEFAULT);
         $user->role = 1; //On met le role utilisateur par défaut
         return $user->save();
     }
     public function verifyCredentials(string $email, string $password): User {
         try {
-            $user = User::query()->where('user_id', $email)->first();
+            $user = User::query()->where('email', $email)->first();
             if ($user && password_verify($password, $user->password)) {
                 return $user;
             }
--- lachaudiere/src/conf/routes.php
+++ lachaudiere/src/conf/routes.php
@@ -1,11 +1,13 @@
 <?php
 declare(strict_types=1);
 
-use lachaudiere\appli\webui\actions\AddEvenementAction;
-use lachaudiere\appli\webui\actions\RegisterAction;
-use lachaudiere\appli\webui\actions\SignoutAction;
+use lachaudiere\webui\actions\AddEvenementAction;
+use lachaudiere\webui\actions\GetCategorieParIdAction;
+use lachaudiere\webui\actions\GetCategoriesAction;
+use lachaudiere\webui\actions\RegisterAction;
+use lachaudiere\webui\actions\SignoutAction;
 use Slim\App;
-use lachaudiere\appli\webui\actions\SigninAction;
+use lachaudiere\webui\actions\SigninAction;
 
 return function(App $app): App {
     $app->get('/categories', GetCategoriesAction::class);
@@ -27,9 +29,9 @@ return function(App $app): App {
 });
     $app->get('/coffrets', \lachaudiere\appli\webui\actions\GetCoffretsAction::class);
     $app->get('/coffret/{id}', \lachaudiere\appli\webui\actions\GetCoffretDetailAction::class);
-    $app->map(['GET', 'POST'], '/box/create', \lachaudiere\appli\webui\actions\AddEvenementAction::class);
+    $app->map(['GET', 'POST'], '/box/create', AddEvenementAction::class);
     $app->get('/coffret/{coffret_id}/prestation/{id}', \lachaudiere\appli\webui\actions\GetPrestationCoffretAction::class);
-    $app->post('/box/prestation/add', \lachaudiere\appli\webui\actions\AddEvenementAction::class);
+    $app->post('/box/prestation/add', AddEvenementAction::class);
     $app->get('/box/courante', \lachaudiere\appli\webui\actions\GetBoxCouranteAction::class);
     $app->get('/evenement/create', AddEvenementAction::class);
     return $app;
