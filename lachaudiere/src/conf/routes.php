08820514528403dfc3335f099347c50bf3935bdf
Audinot Noah <audinotnoah@gmail.com>
2025-06-06 09:15:02 +0200

    ajout dashboard pour creer  des utilisateurs

--- lachaudiere/src/conf/routes.php
+++ lachaudiere/src/conf/routes.php
@@ -2,23 +2,26 @@
 declare(strict_types=1);
 
 use lachaudiere\appli\webui\actions\AddEvenementAction;
-use lachaudiere\appli\webui\actions\RegisterAction;
-use lachaudiere\appli\webui\actions\SignoutAction;
+use lachaudiere\webui\actions\RegisterAction;
+use lachaudiere\webui\actions\SignoutAction;
+use lachaudiere\webui\actions\DashboardAction;
+use lachaudiere\webui\providers\AuthnProviderInterface;
 use Slim\App;
-use lachaudiere\appli\webui\actions\SigninAction;
+use lachaudiere\webui\actions\SigninAction;
 
 return function(App $app): App {
     $app->get('/categories', GetCategoriesAction::class);
-    $app->map(['GET','POST'], '/signin', SigninAction::class);
+    $app->map(['GET','POST'], '/signin', SigninAction::class)->setName('signin');
     $app->post('/signout', SignoutAction::class);
     $app->map(['GET','POST'], '/register', RegisterAction::class);
     $app->get('/categorie/{id}', GetCategorieParIdAction::class);
+    $app->get('/admin/dashboard',DashboardAction::class)->setName('admin.dashboard');
     $app->get('/prestations', \lachaudiere\appli\webui\actions\GetPrestationsAction::class);
     $app->get('/prestation/{id}', GetPrestationParIdAction::class);
     $app->get('/categories/{id}/prestations', GetPrestationsParCategorieAction::class);
     $app->get('/', function ($request, $response, $args) use ($app) {
         $container = $app->getContainer();
-        $authProvider = $container->get(\lachaudiere\webui\providers\AuthnProviderInterface::class);
+        $authProvider = $container->get(AuthnProviderInterface::class);
         $user = $authProvider->getSignedInUser();
         $view = \Slim\Views\Twig::fromRequest($request);
         return $view->render($response, 'home.twig', [
