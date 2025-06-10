39732527a58028e50b5cb30f849b0475556ee2a8
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-06-10 14:40:37 +0200

    nettoyage

--- lachaudiere.web/lib/ui.js
+++ lachaudiere.web/lib/ui.js
@@ -1,14 +1,5 @@
 import Handlebars from "handlebars";
 
-//Affichage d'une image
-export const displayPicture = (picture) => {
-    //Charger et compiler le template
-    const templateSource = document.querySelector('#photoTemplate').innerHTML;
-    const template = Handlebars.compile(templateSource);
-    const photoContainer = document.querySelector('#la_photo');
-    photoContainer.innerHTML = template({url: 'https://webetu.iutnc.univ-lorraine.fr/'+picture.url.href, title: picture.titre, description: picture.descr, type: picture.type, file: picture.file});
-};
-
 //Affichage de la catégorie de l'image
 export const displayCategory = (category) => {
     //Charger et compiler le template
@@ -17,12 +8,3 @@ export const displayCategory = (category) => {
     const categoryContainer = document.querySelector('#la_categorie');
     categoryContainer.innerHTML = template({nom: category.categorie.nom, description: category.categorie.descr});
 };
-
-//Affichage des commentaires
-export const displayComments = (comments) => {
-    const templateSource = document.querySelector('#commentTemplate').innerHTML;
-    const template = Handlebars.compile(templateSource);
-    const commentsContainer = document.querySelector('#les_commentaires');
-    console.log(comments);
-    commentsContainer.innerHTML = template({commentaires: comments.comments});
-}
