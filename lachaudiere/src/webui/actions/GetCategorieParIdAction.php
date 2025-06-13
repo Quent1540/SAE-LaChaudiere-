8eb716c59b61ef5df6df84db8275138c301ba1b5
Audinot Noah <audinotnoah@gmail.com>
2025-06-13 11:29:14 +0200

    ajout markdown categorie

--- lachaudiere/src/webui/actions/GetCategorieParIdAction.php
+++ lachaudiere/src/webui/actions/GetCategorieParIdAction.php
@@ -6,6 +6,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 use Slim\Exception\HttpNotFoundException;
+// AJOUTER CES USES
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\MarkdownConverter;
 
 //Action afficher une catégorie par son id
 class GetCategorieParIdAction {
@@ -17,12 +21,29 @@ class GetCategorieParIdAction {
 
     public function __invoke(Request $request, Response $response, array $args): Response {
         try {
-            //Récup l'id dans les paramètres de l'URL
             $id = (int) $args['id'];
             $categorie = $this->catalogue->getCategorieById($id);
+            
+            if (!$categorie) {
+                throw new HttpNotFoundException($request, "Catégorie non trouvée.");
+            }
+
+            $config = [
+                'html_input' => 'strip',
+                'allow_unsafe_links' => false,
+            ];
+            $environment = new Environment($config);
+            $environment->addExtension(new CommonMarkCoreExtension());
+            $converter = new MarkdownConverter($environment);
+            
+            $htmlDescription = $converter->convert($categorie->description ?? '')->getContent();
+
             $view = Twig::fromRequest($request);
-            return $view->render($response, 'categorieParId.twig', ['categorie' => $categorie]);
-        } catch (EvenementException $e) {
+            return $view->render($response, 'categorieParId.twig', [
+                'categorie' => $categorie,
+                'html_description' => $htmlDescription
+            ]);
+        } catch (\Exception $e) { 
             throw new HttpNotFoundException($request, $e->getMessage());
         }
     }
